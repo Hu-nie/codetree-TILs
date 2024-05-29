484cc504b7bf0842f5a1584a8404ef1711b63507
Hoon <ehdgns8227@naver.com>
2024-05-29 17:12:59 +0900

    Title: 수의 곱셈, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240529/수의 곱셈/multiplication-of-numbers.py
+++ 240529/수의 곱셈/multiplication-of-numbers.py
@@ -1,1 +1,32 @@
-i, j, k = map(int, input.split())
+import itertools
+
+def find_highest_priority_number(a, b, c):
+
+    numbers = [a, b, c]
+    products = set()
+    
+    for r in range(1, 4):  
+        for combo in itertools.combinations(numbers, r):
+            product = 1
+            for num in combo:
+                product *= num
+            products.add(product)
+    
+
+    odd_numbers = []
+    even_numbers = []
+    
+    for num in products:
+        if num % 2 == 0:
+            even_numbers.append(num)
+        else:
+            odd_numbers.append(num)
+            
+    if odd_numbers:
+        return max(odd_numbers)
+    else:
+        return max(even_numbers)
+
+a, b, c = map(int, input().split())
+
+print(find_highest_priority_number(a, b, c))
